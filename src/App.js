a04f110b515ed7900f77840cd88f4f2728df22ad
henryk1229 <henryk1229@gmail.com>
2019-07-09 15:39:11 -0400

    format display cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import "materialize-css/dist/css/materialize.min.css"
 import NavBar from './containers/NavBar'
 import ProfilePage from './containers/ProfilePage'
 import BreweryContainer from './containers/BreweryContainer'
@@ -13,12 +14,25 @@ class App extends React.Component {
 
   state = {
     allBreweries: [],
-    userBreweries: []
+    userBreweries: [],
+    searchTerm: ''
   }
 
   handleClick = (props) =>{
     this.setState({
       userBreweries: [...this.state.userBreweries, props]
+    })
+  }
+
+  setSearchTerm = (newSearchTerm) =>{
+    this.setState({
+      searchTerm: newSearchTerm
+    })
+  }
+
+  applySearch = () =>{
+    return this.state.allBreweries.filter(brewery=> {
+      return brewery.name.toLowerCase().includes(this.state.searchTerm.toLowerCase())
     })
   }
 
@@ -43,8 +57,12 @@ class App extends React.Component {
           <Route exact path="/login" render={()=> <Login/>}/>
           <Route exact path="/signup" render={()=> <SignUp/>}/>
         </Switch>
+        <SearchBar
+        searchTerm={this.state.searchTerm}
+        setSearchTerm={this.setSearchTerm}
+        />
         <BreweryContainer
-         allBreweries={this.state.allBreweries}
+         allBreweries={this.applySearch()}
          handleClick={this.handleClick}/>
       </React.Fragment>
     )
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import "materialize-css/dist/css/materialize.min.css"
 
 class SearchBar extends React.Component {
+
+  handleChange = (event) => {
+    this.props.setSearchTerm(event.target.value)
+  }
+
   render() {
     return(
-      <div>SearchBar</div>
+      <div>
+      <form className="search-form">
+        <input
+          name="searchTerm"
+          placeholder="Find Your Brew..."
+          value={this.props.searchTerm}
+          onChange={this.handleChange}
+          className="input"/>
+      </form>
+      </div>
     )
   }
 }
